f75e18223b22b72a94418bd1938e03b682c2d0ac
AL-AMIN <alamin50cse.miu@gmail.com>
2024-06-09 10:16:44 +0600

    photo circle edit

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -19,18 +19,18 @@ const Photo = () => {
                     transition: { delay: 2, duration: 0.2, ease: "easeInOut" },
                 }}
 
-                className="w-[298px] h-[298px] lg:w-[398px] lg:h-[398px] mix-blend-lighten absolute">
+                className="w-[415px] h-[415px] xl:w-[510px] xl:h-[510px] mix-blend-lighten absolute">
                 <Image src="/Al-Amin__web_.png" priority quality={100} fill alt="" className="object-contain"></Image>
             </motion.div>
             {/* circle */}
-            <motion.svg className="w-[500px] lg:w-[640] h-[500px] lg:h-[640]"
+            <motion.svg className="w-[415px] xl:w-[500px] h-[415px] xl:h-[500px]"
              fill="transparent"
-             viewBox="0 0 506 506"
+             viewBox="0 0 412 412"
              xmlns="http://www.w3.org/2000/svg"
              >
                 <motion.circle
-                cx="203"
-                cy="216"
+                cx="202"
+                cy="210"
                 r="200"
                 stroke="#00ff99"
                 strokeWidth="4"
