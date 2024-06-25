5723bfd10ec96788419f012e887cf90221cfddf3
Al-Amin <alamin50cse.miu@gmail.com>
2024-06-25 17:45:08 +0600

    speliing update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const contact = () => {
     return (
         <div>
-           <p className='text-accent border-spacing-4'> This page under Constractions!!!</p>
+           <p className='text-accent border-spacing-4'> This page under Constructions!!!</p>
            <h1>Name: Al-Amin</h1>
            <h1>Email: alamin50cse.miu@gmail.com</h1>
            <h1>Phone: 01919123903</h1>
