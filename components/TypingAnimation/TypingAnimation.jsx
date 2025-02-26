a2c54db411034685eeb5f7b6eaba4f6abdd6c0b7
alaminwbsoft <wbsoft.alamin@gmail.com>
2025-02-27 00:38:39 +0600

    add

--- components/TypingAnimation/TypingAnimation.jsx
+++ components/TypingAnimation/TypingAnimation.jsx
@@ -2,22 +2,22 @@
 import Typewriter from "typewriter-effect";
 
 const TypingAnimation = () => {
-    return (
-        <div className="text-lg">
+  return (
+    <div className="text-lg">
       <Typewriter
         options={{
           autoStart: true,
           loop: true,
           delay: 40,
           strings: [
-            "I am a Front-end Developer",
             "I am a MERN Stack Developer",
+            "I am a Front-end Developer",
             "I am a React JS Developer",
           ],
         }}
       />
     </div>
-    );
+  );
 };
 
 export default TypingAnimation;
